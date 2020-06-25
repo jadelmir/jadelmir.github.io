659aac9a7167b22e1bae97aff7977c3f1b1daa91
jad el mir <jadelmir>
2020-06-24 21:39:38 -0400

    Updates

---
+++ precache-manifest.e9c0b67d2a44fe1a1a9da501051e9323.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fea2111c5f8781ec27a7ec401e9e322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d3116a86a97bcba3b04",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a15171d8eebb373b3d89",
+    "url": "/static/js/2.948fa599.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.948fa599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3116a86a97bcba3b04",
+    "url": "/static/js/main.5e4d5eb9.chunk.js"
+  },
+  {
+    "revision": "b6685536b3fb649082f3",
+    "url": "/static/js/runtime-main.d4ad8ff7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
